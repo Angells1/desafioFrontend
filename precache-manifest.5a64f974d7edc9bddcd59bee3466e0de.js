c12062c61a240445c0e3cd9d35dc1c61cef8f8ac
gabri <gabriel.angelsad1@gmail.com>
2020-03-06 04:31:40 -0300

    Updates

---
+++ precache-manifest.5a64f974d7edc9bddcd59bee3466e0de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516c5c78e32ea00351ceea90a3a43ced",
+    "url": "/desafioFrontend/index.html"
+  },
+  {
+    "revision": "bb7fde0087faf7c9f5ae",
+    "url": "/desafioFrontend/static/css/main.320f6465.chunk.css"
+  },
+  {
+    "revision": "ee3c1332296ff0f678ca",
+    "url": "/desafioFrontend/static/js/2.2fefe466.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/desafioFrontend/static/js/2.2fefe466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7fde0087faf7c9f5ae",
+    "url": "/desafioFrontend/static/js/main.9299becb.chunk.js"
+  },
+  {
+    "revision": "aa8e97d219d119b399fb",
+    "url": "/desafioFrontend/static/js/runtime-main.5e876a0d.js"
+  },
+  {
+    "revision": "aecd1048777b317c4e4c2c4dd881e2eb",
+    "url": "/desafioFrontend/static/media/Capturar.aecd1048.JPG"
+  }
+]);
