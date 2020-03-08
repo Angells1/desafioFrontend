4f32ec5b0ec54f4032281a602d7b222785853917
gabri <gabriel.angelsad1@gmail.com>
2020-03-07 21:00:01 -0300

    make responsive stuff

--- src/App.js
+++ src/App.js
@@ -1,79 +1,143 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Header, Title, Content, City, WeatherBox, SpnCity,
   InfoTitle, Close, DetailInfo, Info, InfoTemp, End, Start, WeekItem,
-Day, TempDay} from './style.js';
+Day, TempDay, CityInp, Img, Middle, Footer, TitleMiddle,
+LeftList, RightList, Lists, MinLi, MaxLi, CityLi} from './style.js';
 import { IoIosClose } from "react-icons/io";
 import SearchIcon from './Capturar.JPG';
 import { MdArrowUpward } from "react-icons/md";
 import { MdArrowDownward } from "react-icons/md";
-
+import { IoIosSearch } from "react-icons/io";
+import image_path from './search-24px.svg';
 
 function App() {
+
+  function handleDisplay(e) {
+    e.preventDefault();
+    showComp ? setShowComp(false): setShowComp(true);
+
+
+  
+  }
+
+  const [showComp, setShowComp] = useState(true);
+
+
   return (
     <Content>
 <Header>
- 
   <Title>Previsão do tempo</Title>
-  <WeatherBox>
-    <Start>
-    <SpnCity>Niterói, Rj - Brasil</SpnCity>
-    <Close><IoIosClose size='35px'/></Close>
-    <InfoTitle>20°C Nublado</InfoTitle>
-    <DetailInfo>
-     <InfoTemp><MdArrowDownward size='25px' color='orange'/>16°
-     <MdArrowUpward size='25px' color='orange'/>25°
-    </InfoTemp>
-    <Info>
-      Sensação &nbsp; 
-      <strong>
-       19°C
-      </strong>
-    </Info>
-    <Info>
-      Vento &nbsp; 
-      <strong>
-       18km/h
-      </strong>
-    </Info>
-    <Info>
-      Humidade &nbsp; 
-      <strong>
-       89%
-      </strong>
-    </Info>
-    </DetailInfo>
-    </Start>
-    <End>
-    <WeekItem>
-      <Day>Terça</Day>
-      <TempDay>18° 26°</TempDay>
-    </WeekItem>
-    <WeekItem>
-    <Day>Quarta</Day>
-    <TempDay>18° 26°</TempDay>
-    </WeekItem>
-    <WeekItem>
-    <Day>Quinta</Day>
-    <TempDay>18° 26°</TempDay>
-    </WeekItem>
-    <WeekItem>
-    <Day>Sexta</Day>
-    <TempDay>18° 26°</TempDay>
-    </WeekItem>
-    <WeekItem>
-    <Day>Sábado</Day>
-    <TempDay>18° 26°</TempDay>
-    </WeekItem>
-    </End>
-  </WeatherBox>
+  {showComp ? (					
+				 <WeatherBox>
+         <Start>
+         <SpnCity>Niterói, Rj - Brasil</SpnCity>
+         <Close><IoIosClose size='35px' onClick={handleDisplay}/></Close>
+         <InfoTitle>20°C Nublado</InfoTitle>
+         <DetailInfo>
+          <InfoTemp><MdArrowDownward size='25px' color='orange'/>16°
+          <MdArrowUpward size='25px' color='orange'/>25°
+         </InfoTemp>
+         <Info>
+           Sensação &nbsp; 
+           <strong>
+            19°C
+           </strong>
+         </Info>
+         <Info>
+           Vento &nbsp; 
+           <strong>
+            18km/h
+           </strong>
+         </Info>
+         <Info>
+           Humidade &nbsp; 
+           <strong>
+            89%
+           </strong>
+         </Info>
+         </DetailInfo>
+         </Start>
+         <End>
+         <WeekItem>
+           <Day>Terça</Day>
+           <TempDay>18° 26°</TempDay>
+         </WeekItem>
+         <WeekItem>
+         <Day>Quarta</Day>
+         <TempDay>18° 26°</TempDay>
+         </WeekItem>
+         <WeekItem>
+         <Day>Quinta</Day>
+         <TempDay>18° 26°</TempDay>
+         </WeekItem>
+         <WeekItem>
+         <Day>Sexta</Day>
+         <TempDay>18° 26°</TempDay>
+         </WeekItem>
+         <WeekItem>
+         <Day>Sábado</Day>
+         <TempDay>18° 26°</TempDay>
+         </WeekItem>
+         </End>
+       </WeatherBox>
+				) : <></>}
+ 
  
 
 
-  <City 
-  background={SearchIcon}
-  
-  />
+  <City>
+    <CityInp ></CityInp>
+    <Img background={image_path} onClickCapture={handleDisplay}></Img>
+  </City>
   </Header>
+  <Middle>
+         
+    
+    <TitleMiddle>Capitais</TitleMiddle>
+  <Lists>
+
+    <LeftList>
+  
+        <MinLi>18°</MinLi>
+        <MaxLi>27°</MaxLi>
+        <CityLi>Rio de Janeiro</CityLi>
+        <MinLi>14°</MinLi>
+        <MaxLi>22°</MaxLi>
+        <CityLi>São Paulo</CityLi>
+        <MinLi>21°</MinLi>
+        <MaxLi>22°</MaxLi>
+        <CityLi>Belo horizonte</CityLi>
+        <MinLi>24°</MinLi>
+        <MaxLi>37°</MaxLi>
+        <CityLi>Brasília</CityLi>
+        <MinLi>24°</MinLi>
+        <MaxLi>27°</MaxLi>
+        <CityLi>Bélem</CityLi>
+        
+    </LeftList>
+    <RightList>
+   
+    <MinLi>23°</MinLi>
+        <MaxLi>37°</MaxLi>
+        <CityLi>Salvador</CityLi>
+        <MinLi>5°</MinLi>
+        <MaxLi>14°</MaxLi>
+        <CityLi>Curitiba</CityLi>
+        <MinLi>21°</MinLi>
+        <MaxLi>32°</MaxLi>
+        <CityLi>Fortaleza</CityLi>
+        <MinLi>24°</MinLi>
+        <MaxLi>37°</MaxLi>
+        <CityLi>Manaus</CityLi>
+        <MinLi>28°</MinLi>
+        <MaxLi>40°</MaxLi>
+        <CityLi>João Pessoa</CityLi>
+     
+    </RightList>
+    
+    </Lists>  
+    </Middle>
+  <Footer>Feito com ❤️ por Gabriel Angelo</Footer>
   </Content>
 
   );
--- src/style.js
+++ src/style.js
@@ -1,19 +1,67 @@
 import styled from 'styled-components';
+
 import { IoIosSearch } from "react-icons/io";
+import media from './media';
+var image_path = './search-24px.svg';
 
 export const Header =  styled.div`
 
-padding: 30px 15px 30px;
+
+
+padding: 20px 15px 10px;
 border-bottom: 1px solid #fff;
 text-align: center;
 font-family: sans-serif;
 width: 550px;
+
+
+
 letter-spacing: 1px;
 margin: 0 auto;
+min-width: 310px;
+
+
+
+@media (max-width: 585px){
+    width: 100%;
+    padding: 20px 0px 10px;
+   
+    
+}
+
+@media (max-width: 435px){
+   
+    padding-top: 2px;
+    
+}
+
+
+
+
+
 `;
 
-export const Content =  styled.div`
 
+/*${media.babybear`
+     width: 100%;
+    background: red;
+     border-bottom: 1px solid #000;
+   `}*/ 
+export const Content =  styled.div`
+background: linear-gradient(#ff7b00, #ffbb00) no-repeat;
+
+
+min-height: 100vh;
+display: flex;   
+flex-direction: column;
+justify-content: space-between;
+
+
+@media (max-width: 470px){
+   
+    width: 100%;
+    
+ }
 
 
 `;
@@ -21,15 +69,21 @@ export const Content =  styled.div`
 
 export const Title = styled.h1`
 
-
+font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 color: #fff;
-font-size: 2.6em;
+font-size: 2.8em;
 
+@media (max-width: 435px){
+   
+   font-size: 1.8em;
+    
+ }
 
 
 
 
 `;
+
 
 
 
@@ -40,6 +94,7 @@ export const WeatherBox = styled.div.attrs({
 })`
 position: relative;
 background: #fff3e4;
+
 width: 80%;
 padding: 20px 10px;
 box-sizing: border-box;
@@ -47,29 +102,101 @@ height: 270px;
 margin: 15px auto;
 text-align: start;
 
+
+
+
+@media (max-width: 580px){
+    width: 100%;
+    margin: auto;
+    box-shadow: inset 0 4px 15px rgba(0,0,0,.25);
+}
+
+
+@media (max-width: 435px){
+    width: 100%;
+    padding: 30px 0;
+    margin: auto;
+ 
+}
+
+@media (max-width: 330px){
+ 
+    padding: 10px 0;
+
+}
+
+
 `;
 
 
-export const City = styled.input.attrs({
+export const City = styled.form.attrs({
+    
+})`
+position: relative;
+width: 70%;
+text-align: start;
+font-size: 1.2em;
+font-weight: 100;
+margin: 10px auto;
+display: flex;
+border: 1px solid #999;
+background-color: #fff;
+
+@media (max-width: 580px){
+    width: 90%;
+    margin: 20px auto;
+ 
+   
+}
+
+
+
+
+`
+
+
+export const CityInp = styled.input.attrs({
     type: ('text'),
     placeholder: ('Insira aqui o nome da Cidade')
 })`
 
-width: 75%;
+width: 85%;
 padding: 10px;
 font-size: 1.2em;
 font-weight: 100;
-margin: 10px auto;
+border: none;
 background: #fff url('${props => `${props.background}`}') no-repeat right center;
-border: 1px solid #999;
+
+
 
 
 ::placeholder {
     color: #949494;
+    font-size: 1em;
    
 }
 
+
 `
+
+export const Img = styled.a.attrs({
+  href:('')
+})`
+
+
+
+width: 15%;
+
+  background: url('${props => `${props.background}`}') no-repeat center;  
+    background-size: 30px;
+    transition: background-color 0.3s linear;
+
+    :hover {
+    background-color: #e0e0e0;
+}
+
+
+`;
 
 
 export const SpnCity = styled.span.attrs({
@@ -105,6 +232,19 @@ font-weight: bold;
 color: #4b4b4b;
 margin: 20px 0;
 
+
+
+@media (max-width: 435px){
+    font-size: 35px;
+    margin: 10px 0;
+}
+
+@media (max-width: 330px){
+    margin: 5px 0 1px;
+    font-size: 33px;
+
+}
+
 `;
 
 
@@ -116,6 +256,21 @@ display: grid;
 grid-column-gap: 15px;
 grid-template-columns: 1fr 1fr;
 width: 75%;
+
+
+
+@media (max-width: 435px){
+    grid-column-gap: 2px;
+    width: 100%;
+}
+
+@media (max-width: 330px){
+    grid-template-columns: 1fr;
+    grid-column-gap: 5px;
+   margin-bottom: 8px;
+    width: 100%;
+
+}
 
 
 `;
@@ -134,6 +289,14 @@ color: #5b5b5b;
 font-family: sans-serif;
 text-align:center;
 
+
+@media (max-width: 330px){
+   
+    line-height: 30px;
+
+}
+
+
 `;
 
 export const InfoTemp = styled.span.attrs({
@@ -145,6 +308,11 @@ export const InfoTemp = styled.span.attrs({
 color: #5b5b5b;
 font-weight: bold;
 
+@media (max-width: 330px){
+    text-align: center;
+    line-height: 20px;
+
+}
 
 
 `;
@@ -183,6 +351,12 @@ display: flex;
 flex-direction: row;
 justify-content: space-around;
 
+@media (max-width: 330px){
+   
+    padding: 10px 3px;
+}
+
+
 `;
 
 export const WeekItem = styled.div.attrs({
@@ -197,6 +371,7 @@ flex-direction: column;
 flex-wrap: nowrap;
 
 
+
 `;
 
 export const Day = styled.span.attrs({
@@ -205,6 +380,16 @@ export const Day = styled.span.attrs({
 
 font-weight: bold;
 color: #5b5b5b;
+
+
+
+@media (max-width: 330px){
+   
+   font-size: 0.9em;
+    text-indent: 2px;
+
+
+}
 
 
 `;
@@ -218,4 +403,147 @@ color: #ff8800;
 margin-top: 5px;
 font-size: 0.9em;
 
+@media (max-width: 330px){
+   
+    font-weight: 300;
+    font-size: 0.8em;
+    text-indent: 2px;
+}
+
 `;
+
+
+export const Middle = styled.main`
+
+
+padding: 2px 8px;
+box-sizing: border-box;
+width: 450px;
+height: 100%;
+
+
+margin: 0 auto;
+
+@media (max-width: 452px){
+ 
+    width: 100%;
+ 
+   
+    
+}
+
+
+`
+
+export const TitleMiddle = styled.h3`
+
+font-size: 2em;
+color: #fff;
+
+margin: 15px 0;
+
+`
+
+
+export const Footer = styled.footer`
+
+
+
+margin-top: 20px;
+width: 100%;
+height: 100px;
+background: rgba(0, 0, 0, 0.3);
+display: flex;
+justify-content: center;
+align-items: center;
+color: #fff;
+box-sizing: border-box;
+min-width: 310px;
+`;
+
+export const Lists = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-column-gap: 20px;
+    color: #282828;
+    font-size:  15px;
+    font-family: sans-serif;
+    font-weight: 600;
+    margin: 0 auto;
+    
+    
+
+@media (max-width: 476px){
+    width: 100%;
+    margin: 0 auto;
+    
+}
+
+
+    @media (max-width: 455px){
+   
+        grid-template-columns: 1fr;
+        margin: auto;
+ 
+    
+}
+
+
+`;
+
+export const LeftList = styled.ul`
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr 4fr;
+    list-style-type: none;
+    grid-row-gap: 20px;
+   
+
+
+    @media (max-width: 445px){
+   
+        margin-bottom: 2px;
+        width: 70%;
+        margin: 5px auto;
+    }
+
+`;
+
+export const RightList = styled.ul`
+    padding: 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr 4fr;
+    list-style-type: none;
+   
+    grid-row-gap: 20px;
+   
+    @media (max-width: 445px){
+   
+       
+        width: 70%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+
+    
+`;
+
+export const MinLi = styled.li`
+
+
+
+ `;
+ 
+
+export const MaxLi = styled.li`
+
+ 
+ 
+ 
+ `;
+
+export const CityLi = styled.li`
+
+
+ 
+ `;
